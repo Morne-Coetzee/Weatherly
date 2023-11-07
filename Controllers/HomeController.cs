fb8fa792162cda112c47cba0fa6ee93446c8d4d3
Morne Coetzee <morne.mornecoetzee@gmail.com>
2023-11-07 18:00:46 +0200

    Main Wheatherly commit
    Main Wheatherly commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Wheatherly.Models;  // Replace with your actual model classes.
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Wheatherly.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public class WeatherData
+        {
+            public Location Location { get; set; }
+            public CurrentData Current { get; set; }
+
+            public WeatherData()
+            {
+                Location = new Location();
+                Current = new CurrentData();
+            }
+        }
+
+        public class Location
+        {
+            public string Name { get; set; } = "";
+            public string Region { get; set; } = "";
+            public string Country { get; set; } = "";
+            public double Lat { get; set; } = 0.0;
+            public double Lon { get; set; } = 0.0;
+            public string Tz_id { get; set; } = "";
+            public long Localtime_epoch { get; set; } = 0;
+            public string Localtime { get; set; } = "";
+        }
+
+        public class CurrentData
+        {
+            public long Last_updated_epoch { get; set; } = 0;
+            public string Last_updated { get; set; } = "";
+            public double Temp_c { get; set; } = 0.0;
+            public double Temp_f { get; set; } = 0.0;
+            public int Is_day { get; set; } = 0;
+            public Condition Condition { get; set; } = new Condition();
+            public double Wind_mph { get; set; } = 0.0;
+            public double Wind_kph { get; set; } = 0.0;
+            public int Wind_degree { get; set; } = 0;
+            public string Wind_dir { get; set; } = "";
+            public double Pressure_mb { get; set; } = 0.0;
+            public double Pressure_in { get; set; } = 0.0;
+            public double Precip_mm { get; set; } = 0.0;
+            public double Precip_in { get; set; } = 0.0;
+            public int Humidity { get; set; } = 0;
+            public int Cloud { get; set; } = 0;
+            public double Feelslike_c { get; set; } = 0.0;
+            public double Feelslike_f { get; set; } = 0.0;
+            public double Vis_km { get; set; } = 0.0;
+            public double Vis_miles { get; set; } = 0.0;
+            public double Uv { get; set; } = 0.0;
+            public double Gust_mph { get; set; } = 0.0;
+            public double Gust_kph { get; set; } = 0.0;
+        }
+
+        public class Condition
+        {
+            public string Text { get; set; } = "";
+            public string Icon { get; set; } = "";
+            public int Code { get; set; } = 0;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeather(string location, string unit)
+        {
+            try
+            {
+                var apiKey = "a1569387c72e47d4a4f141504230611";
+                var baseUrl = "https://api.weatherapi.com/v1/current.json";
+                using var httpClient = _httpClientFactory.CreateClient();
+                var response = await httpClient.GetAsync($"{baseUrl}?key={apiKey}&q={location}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var weatherData = JsonConvert.DeserializeObject<WeatherData>(json);
+
+                    if (weatherData != null && !string.IsNullOrEmpty(weatherData.Location?.Name))
+                    {
+                        return Ok(weatherData);
+                    }
+                    else
+                    {
+                        return BadRequest("Location not available in the weather data.");
+                    }
+                }
+
+                return BadRequest("Failed to retrieve weather data from the external API.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:", ex);
+                return StatusCode(500, "An error occurred while fetching weather data.");
+            }
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Wheatherly
-Wheatherly
+# Weather.ly - Wheather without hastles
+
+![Weather.ly Logo](/wwwroot/img/1-no-bg.png)
+
+Weather.ly is a web application that allows you to retrieve real-time weather information for your desired location. This README provides an overview of the application and instructions for running it.
+
+## Features
+
+- Retrieve current weather details from around the world.
+- Choose between Fahrenheit (°F) and Celsius (°C) units.
+- Toggle between dark and light mode for a comfortable viewing experience.
+- Responsive design for both desktop and mobile devices.
+
+## Getting Started
+
+To run Weather.ly locally on your machine, follow these steps:
+
+1. **Clone the Repository:**
+git clone https://github.com/Morne-Coetzee/Wheatherly.git
+
+2. **Navigate to the Project Directory:**
+cd Weather.ly
+
+3. **Restore Dependencies:**
+dotnet restore
+
+4. **Build the Project:**
+dotnet build
+
+5. **Run the Application:**
+dotnet run
+
+6. **Access the Application:**
+Open your web browser and navigate to the URL where the application is hosted. By default, it's often `https://localhost:5001` or `http://localhost:5000`. Check the console output for the exact URL.
+
+## Usage
+
+1. Enter the location (e.g., Pretoria) for which you want to retrieve weather information.
+2. Choose between Fahrenheit (°F) and Celsius (°C) units.
+3. Click the "Get Weather" button to fetch the weather data.
+4. Enjoy the weather information with an option to switch between dark and light mode.
+
+## Technologies Used
+
+- .NET Core for the back-end.
+- HTML, CSS, and Bootstrap for the front-end.
+- WeatherAPI.com for weather data.
+
+## Acknowledgments
+
+- Thanks to WeatherAPI.com for providing the weather data.
+- Inspired by the need for a simple and informative weather app.
+- Created for King Price staff as an assesment.
+
+## Side Notes
+
+Please feel free to send me an email if there is any stuggles or questions, I will get back as soon as possible.
+Email: morne.mornecoetzee@gmail.com
+Cell: 0761884899
